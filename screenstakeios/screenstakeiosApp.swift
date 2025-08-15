5e75f9191926c30e68fff1f69e762dcb0d33088b
andrewgrover <andygrover@optonline.net>
2025-08-14 20:43:05 -0400

    init

---
+++ screenstakeios/screenstakeiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  screenstakeiosApp.swift
+//  screenstakeios
+//
+//  Created by Andrew Grover on 8/14/25.
+//
+
+import SwiftUI
+
+@main
+struct screenstakeiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
